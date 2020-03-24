83b6ed9d97d03dc26fe101467146b1fa354028cc
vandinh124 <vanvdinh124@gmail.com>
2020-03-24 15:22:19 -0700

    initial commit

--- belt_exam/settings.py
+++ belt_exam/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'a7*5@&=c0*x01uekup+qg=-853-%_bwxl0_(7zcm)r(!52p!k9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['{{AWSIP}}']
 
 
 # Application definition
@@ -119,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
